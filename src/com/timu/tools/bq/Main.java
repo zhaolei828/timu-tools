c3edcf7d325da4144bd604e3450447315bc964c8
zhaolei <zhaolei@chinadrtv.com>
2016-08-10 01:07:36 +0800

    2

--- src/com/timu/tools/bq/Main.java
+++ src/com/timu/tools/bq/Main.java
@@ -1,6 +1,14 @@
 package com.timu.tools.bq;
 
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -10,7 +18,7 @@ import java.util.Map;
  * Time: 下午11:42
  */
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         /**
          * 题号、分类、学段、年级、一级知识点、二级知识点、三级知识点、四级知识点
          * 难度、能力结构、题型、题干、解答、解析
@@ -30,6 +38,19 @@ public class Main {
         resultMap.put("四级知识点","");
         resultMap.put("试题评价","");
         resultMap.put("能力结构","");
-
+        InputStream is = new FileInputStream("F:\\words\\bq\\1.docx");
+        XWPFDocument doc = new XWPFDocument(is);
+        List<XWPFParagraph> paras = doc.getParagraphs();
+        for (XWPFParagraph para : paras) {
+            //当前段落的属性
+            System.out.println(para.getText());
+        }
+        if (is != null) {
+            try {
+                is.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
